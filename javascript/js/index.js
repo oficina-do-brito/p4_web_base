ab9d8d0e2b1b745021c425197d000234b4dd84eb
oficina-do-brito <clodoaldo.brtp4@gmail.com>
2023-06-02 13:13:52 -0300

    more about js

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,2 +1,9 @@
+/**
+ * @description Uma função que reccebe como parametro  numeros e realiza sua soma,
+ * @param {number} nota1
+ * @param {number} nota2
+ * @returns {number}
+ * Ainda tem referencias de chamada de outras funçoes, caso necessario
+ */
 const funcao = (nota1,nota2)=> nota1+nota2
 console.log(funcao(4,7))
