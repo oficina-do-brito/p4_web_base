799a6c790b1fbc85235514f2b68b5a9e841526c1
oficina-do-brito <clodoaldo.brtp4@gmail.com>
2023-06-03 10:26:33 -0300

    created string content

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -7,3 +7,16 @@
  */
 const funcao = (nota1,nota2)=> nota1+nota2
 console.log(funcao(4,7))
+
+
+var stringona = "meu nome é ".concat("Amaral");
+let primeiraString = ["vazia"];
+let segundaString= ["vazia"];
+let args= ["vazia"];
+
+//ignore a desestruturação por enquanto
+[primeiraString,segundaString, ...args] = stringona.split(" ");
+console.log(primeiraString);
+console.log(segundaString);
+console.log(args);
+console.log(stringona.split(" "));
---
+++ javascript/js/numeros.js
@@ -0,0 +1,11 @@
+document.getElementById("numeros").innerHTML = `
+<p> </p>
+<ul>
+    <li><b></b>:</li>
+    <pre>
+        <code>
+            
+        </code>
+    </pre>
+</ul>
+`
