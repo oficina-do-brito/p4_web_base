6537d352e8519d8e085559fb38262b1d3ee6d4a1
oficina-do-brito <clodoaldo.brtp4@gmail.com>
2023-06-07 12:03:42 -0300

    add session typest ans OO

---
+++ typescript/src/ts/regras.ts
@@ -0,0 +1,54 @@
+document.getElementById('basico').innerHTML = `
+    <h1> Bem vindo ao mundo javascript com utilização do typescript <h1>
+    </br></br></br>
+    <p>Para utilização do typescrip precisamos do node.js instalado, mais precisamente do instalador de pacotes dele o npm:</p>
+    <pre>
+        <code>
+            npm install -g typescript
+        </code>
+        </pre>
+    <p>Para iniciar um mini projeto com typescript basta que a gente crie o seu arquivo de configuração do typescript e isso é feito a partir do comando de iniciação:</p>
+    <pre>
+        <code>
+            tsc --init 
+        </code>
+    </pre>
+    <p>E para iniciar projeto com javascript e  registrar as dependencias, criar package.json:</p>
+    <pre>
+        <code>
+            npm init -y
+        </code>
+    </pre>
+
+    <p>E pra fazer o typescript transpilar e virar javascript?</p>
+    <pre>
+        <code>
+        tsc
+        </code>
+    </pre>
+    
+    <p>E por que utilizar typescript nos nossos projetos?</p>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    
+    </ul>
+
+
+`
+/**
+ * <pre>
+        <code>
+            
+        </code>
+        </pre>
+    <p></p>
+    <p></p>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    
+    </ul>
+ */
